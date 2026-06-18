3b5f64226934ac2805f0703f88c446e4d78e9a60
Kronik <sanekmuvikitranslacii@gmail.com>
2026-06-18 18:52:10 +0300

    CustomTPA v1.6.0 source code – Universal Paper & Spigot/Bukkit

---
+++ src/main/spigot/ru/mcplugins/tpa/util/AdventureProvider.java
@@ -0,0 +1,8 @@
+package ru.mcplugins.tpa.util;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AdventureProvider {
+    public static void create(JavaPlugin plugin) { }
+    public static void close() { }
+}
