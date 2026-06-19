c7512fa7c72aea1398499b0dfa25bd938e80bddf
Kronik <sanekmuvikitranslacii@gmail.com>
2026-06-19 11:44:05 +0300

    Delete src/main directory

--- src/main/spigot/ru/mcplugins/tpa/util/AdventureProvider.java
+++
@@ -1,8 +0,0 @@
-package ru.mcplugins.tpa.util;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class AdventureProvider {
-    public static void create(JavaPlugin plugin) { }
-    public static void close() { }
-}
